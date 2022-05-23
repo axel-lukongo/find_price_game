c0ad24be551031d864db9233fbe5af35a259c0c3
axel lukongo <axellukongo@MacBook-Air-de-axel.local>
2022-05-23 17:54:25 +0200

    finished

--- find_prince.py
+++ find_prince.py
@@ -1,18 +1,21 @@
 import random
 from unicodedata import name
 
-#print("welcom in just price")
+#print("first i ask if to click 1 for start")
 beggin = str(input("welcom in just price, enter 1 for start: "))
 # i print something for when i input "1"
-print("so you press " + beggin + " we can start")
-#in this part i make the just price
+print("you press " + beggin + "so we can start")
+#in this part i creat a random value and the player have to guess it
 ran = random.randint(0, 10000)
 value = input("write a number: ")
 i = 0
-while value != ran:
+while value != ran and i < 7:
 	if value < ran: print("greatter")
 	else: print("smallest")
 	value = input("write a number: ")
 	i = i + 1
 i = str(i)
-print("you won in "+ i + " tour")
+if i < 7: 
+	print("you won in "+ i + " tour")
+else : 
+	print("game over")
